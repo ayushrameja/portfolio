766867a3f05b87d4129bd0aae601fdedf278bd2e
ayushrameja <ayushrwork@gmail.com>
2026-02-04 16:24:38 +0530

    refactor(favicon): Added new favicon and logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,10 +5,6 @@ import ClientLayout from "./ClientLayout";
 export const metadata: Metadata = {
   title: "Ayush Rameja | Portfolio",
   description: "Software Engineer at Autodesk building fast, thoughtful web experiences",
-  icons: {
-    icon: "/favicon.ico",
-    apple: "/apple-touch-icon.png",
-  },
 };
 
 export default function RootLayout({
@@ -19,6 +15,12 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth" suppressHydrationWarning>
       <head>
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="Ayush Rameja" />
+        <link rel="manifest" href="/site.webmanifest" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
